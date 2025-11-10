f26688931a852260fb1496cb027213661bd407ad
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-11-09 21:15:01 -0500

    desktop grid layout complete

--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,42 @@ export default function Home() {
       className="
         px-[35px] py-[73]
         grid grid-cols-1 gap-8
-        md:max-lg:grid-cols-2
+        md:grid-cols-2
+        xl:grid-cols-4
       "
     >
       <TestimonialCard
         author="daniel"
         className="
-          md:max-lg:row-start-1 md:max-lg:col-start-1 md:max-lg:col-span-2
+          md:row-start-1 md:col-start-1 md:col-span-2
         "
       />
       <TestimonialCard
         author="jonathan"
         className="
-          md:max-lg:row-start-2 md:max-lg:col-start-1 md:max-lg:col-span-1
+          md:row-start-2 md:col-start-1 md:col-span-1
+          xl:row-start-1 xl:col-start-3
         "
       />
       <TestimonialCard
         author="jeanette"
         className="
-          md:max-lg:row-start-2 md:max-lg:col-start-2 md:max-lg:col-span-1
+          md:row-start-2 md:col-start-2 md:col-span-1
+          xl:col-start-1
         "
       />
       <TestimonialCard
         author="patrick"
         className="
-          md:max-lg:row-start-3 md:max-lg:col-start-1 md:max-lg:col-span-2
+          md:row-start-3 md:col-start-1 md:col-span-2
+          xl:row-start-2 xl:col-start-2
         "
       />
       <TestimonialCard
         author="kira"
         className="
-          md:max-lg:row-start-4 md:max-lg:col-start-1 md:max-lg:col-span-2
+          md:row-start-4 md:col-start-1 md:col-span-2
+          xl:row-start-1 xl:col-start-4 xl:col-span-1 xl:row-span-2
         "
       />
     </main>
