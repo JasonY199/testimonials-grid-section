20be567d0e9c32a2ce90ec158eabe458d4d1c032
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-11-09 21:08:12 -0500

    tablet grid layout complete

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,39 @@ export default function Home() {
       className="
         px-[35px] py-[73]
         grid grid-cols-1 gap-8
+        md:max-lg:grid-cols-2
       "
     >
-      <TestimonialCard author="daniel" />
-      <TestimonialCard author="jonathan" />
-      <TestimonialCard author="jeanette" />
-      <TestimonialCard author="patrick" />
-      <TestimonialCard author="kira" />
+      <TestimonialCard
+        author="daniel"
+        className="
+          md:max-lg:row-start-1 md:max-lg:col-start-1 md:max-lg:col-span-2
+        "
+      />
+      <TestimonialCard
+        author="jonathan"
+        className="
+          md:max-lg:row-start-2 md:max-lg:col-start-1 md:max-lg:col-span-1
+        "
+      />
+      <TestimonialCard
+        author="jeanette"
+        className="
+          md:max-lg:row-start-2 md:max-lg:col-start-2 md:max-lg:col-span-1
+        "
+      />
+      <TestimonialCard
+        author="patrick"
+        className="
+          md:max-lg:row-start-3 md:max-lg:col-start-1 md:max-lg:col-span-2
+        "
+      />
+      <TestimonialCard
+        author="kira"
+        className="
+          md:max-lg:row-start-4 md:max-lg:col-start-1 md:max-lg:col-span-2
+        "
+      />
     </main>
   );
 }
